8f2f63545b44a0773808ce8c7d7919c201bfc893
Qais Patankar <qaisjp@gmail.com>
2018-03-27 23:47:46 +0100

    tweak stuff

--- try-both.sh
+++ try-both.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -o xtrace
-gcc -Werror  -o mem_sim mem_sim.c -std=gnu99 -lm && ./mem_sim tlb+cache 16 4096 256 32 $1
+gcc -Werror  -o mem_sim mem_sim.c -std=gnu99 -lm && valgrind ./mem_sim tlb+cache 16 4096 256 32 $1
